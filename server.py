35683205255422ed106fec5e27d7582d4f687dc8
Joakim Bergman <joab@kth.se>
2012-09-05 11:17:16 +0200

    Can now read file and set params from CLI

--- server.py
+++ server.py
@@ -1,23 +1,37 @@
 from gevent import monkey
 monkey.patch_all()
 from bottle import get, run, error, abort
+import sys
 import random
+from linecache import getline
 
-number_of_rows = 3141592
-page_size = 100
+port = int(sys.argv[1])
+page_size = int(sys.argv[2])
+filename = sys.argv[3]
+# warm up the cache
+getline(filename, 0)
 
 @error(404)
 def error404(error):
-    return ('API usage:\n'
+    return ('Request any page of {page_size} rows. Searching outside of the\n'
+            'available range will return empty results. The service is\n'
+            'extremly shaky so expect erors.\n\n'
+            'API usage:\n'
             'GET /logs/<page>\n\n'
-            'First page:\n'
-            'GET /logs/0\n\n'
-            'Second page:\n'
-            'GET /logs/1\n')
+
+            'Debugging:\n'
+            'GET /numbers/<page>\n').format(page_size=page_size)
 
 @error(503)
 def error503(error):
     return 'Service Unavailable\n'
+
+@get('/numbers/<page:int>')
+def numbers(page):
+    start = page * page_size
+    end = start + page_size
+
+    return get_numbers(start, end)
 
 @get('/logs/<page:int>')
 def logs(page):
@@ -26,11 +40,18 @@ def logs(page):
         abort(503)
 
     start = page * page_size
-    end = min(start + page_size, number_of_rows)
+    end = start + page_size
 
-    return get_rows(start, end) + '\n'
+    return get_rows(start, end)
 
 def get_rows(start, end):
-    return '\n'.join([str(i) for i in range(start, end)])
+    output = ''
+    for line in xrange(start, end):
+        output += getline(filename, line)
+
+    return output
+
+def get_numbers(start, end):
+    return '\n'.join([str(i) for i in range(start, end)]) + '\n'
 
 run(server='gevent')
